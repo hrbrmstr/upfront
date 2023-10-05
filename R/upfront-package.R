e32a8d660603de51e250dc046daa818f2e04f23c
hrbrmstr <bob@rud.is>
2023-10-05 19:31:07 -0400

    R 📦 repo initialization complete

---
+++ R/upfront-package.R
@@ -0,0 +1,14 @@
+# nolint start
+
+#' ...
+#' 
+#' @md
+#' @name upfront
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+# @import httr
+# @importFrom jsonlite fromJSON
+# @ useDynLib upfront, .registration = TRUE
+"_PACKAGE"
+
+# nolint end
